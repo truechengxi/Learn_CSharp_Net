cfd5e4ab9fb736390e1f13b456addf80ece7ca35
2695171815@qq.com <2695171815@qq.com>
2025-05-22 08:36:51 +0800

    5-22家里

--- Learn_Net_Echo/AsyncServer.cs
+++ Learn_Net_Echo/AsyncServer.cs
@@ -1,7 +1,33 @@
+using System.Net;
+using System.Net.Sockets;
+
 namespace Learn_Net_Echo
 {
     public class AsyncServer
     {
+        private Socket socket;
         
+        public void StartServer()
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            
+        }
+
+        public void BindIpAndPort(string ip, int port )
+        {
+            IPAddress ipAddress = IPAddress.Parse(ip);
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, port);
+            socket.Bind(ipEndPoint);
+        }
+
+        public void Listen()
+        {
+            socket.Listen(10);
+        }
+
+        public void Accept()
+        {
+            
+        }
     }
 }
